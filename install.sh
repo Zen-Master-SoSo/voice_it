9bb660c439cb84426d45a46feebb051774cb9ed3
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2022-03-19 13:04:53 +0800

    Copy .ico, fix perms in installer

--- install.sh
+++ install.sh
@@ -8,12 +8,13 @@ if [ ! -d /usr/lib/python3.8/site-packages/voice-it/ ]
 then
 	sudo mkdir -p /usr/lib/python3.8/site-packages/voice-it/
 fi
-sudo cp voice-it.py voice-it.png voice-it.svg interface.py interface.html /usr/lib/python3.8/site-packages/voice-it/
+sudo cp voice-it.py voice-it.png voice-it.svg interface.py interface.html favicon.ico /usr/lib/python3.8/site-packages/voice-it/
 sudo chmod 0755 /usr/lib/python3.8/site-packages/voice-it/voice-it.py
 sudo chmod 0644 /usr/lib/python3.8/site-packages/voice-it/voice-it.png
 sudo chmod 0644 /usr/lib/python3.8/site-packages/voice-it/voice-it.svg
 sudo chmod 0644 /usr/lib/python3.8/site-packages/voice-it/interface.py
 sudo chmod 0644 /usr/lib/python3.8/site-packages/voice-it/interface.html
+sudo chmod 0644 /usr/lib/python3.8/site-packages/voice-it/favicon.ico
 sudo cp voice-it.desktop /usr/share/applications/voice-it.desktop
 sudo chmod 0644 /usr/share/applications/voice-it.desktop
 sudo /usr/sbin/ufw delete allow in to any port 8585
